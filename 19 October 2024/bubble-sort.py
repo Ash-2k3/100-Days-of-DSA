daa4a196bc87d94f3ad07dbb66d9d53ea4d41280
Ashwath Kannan <ash2k3@Ashwaths-MacBook-Air.local>
2024-10-19 18:51:47 +0530

    Bubble Sort - Early termination Approach

--- 19 October 2024/bubble-sort.py
+++ 19 October 2024/bubble-sort.py
@@ -4,8 +4,11 @@ class Solution:
     def bubbleSort(self, nums):
         n = len(nums)
         for i in range(n):
+            swapped = False
             for j in range(n - 1 - i):
                 if nums[j] > nums[j + 1]:
                     nums[j], nums[j + 1] = nums[j + 1], nums[j]
-
+                    swapped = True
+            if not swapped:
+                break
         return nums
