ad0f7f894adc9ffdeb17416fd4328aeaebdd5f32
Ashwath Kannan <ash2k3@Ashwaths-MacBook-Air.local>
2024-10-19 18:45:44 +0530

    Bubble Sort - Slight improvement for inner loop iteration

--- 19 October 2024/bubble-sort.py
+++ 19 October 2024/bubble-sort.py
@@ -4,7 +4,7 @@ class Solution:
     def bubbleSort(self, nums):
         n = len(nums)
         for i in range(n):
-            for j in range(n - 1):
+            for j in range(n - 1 - i):
                 if nums[j] > nums[j + 1]:
                     nums[j], nums[j + 1] = nums[j + 1], nums[j]
 
