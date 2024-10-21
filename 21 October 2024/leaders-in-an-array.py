190aa57fa1e7123567971fc661927e9eeacd9103
Ash-2k3 <ash.me.cse@gmail.com>
2024-10-21 12:58:52 +0530

    Leaders in an array - Optimal

--- 21 October 2024/leaders-in-an-array.py
+++ 21 October 2024/leaders-in-an-array.py
@@ -1,19 +1,12 @@
 class Solution:
     def leaders(self, nums):
-        leaders_list = []
-
-        # A Flag to check if there's any leader in the current window of elements.
-        leader_found = False
         n = len(nums)
-        for i in range(n):
-            for j in range(i + 1, n):
-                if nums[i] <= nums[j]:
-                    leader_found = False
-                    break
-                leader_found = True
-            if leader_found:
-                leaders_list.append(nums[i])  
-            leader_found = False
+        maxEleInRight = float('-inf')
+        leaders_list = []
+        for i in range(n - 1, -1, -1):
+            if nums[i] > maxEleInRight:
+                maxEleInRight = nums[i]
+                leaders_list.append(maxEleInRight)
 
-        leaders_list.append(nums[-1])
+        leaders_list.reverse()
         return leaders_list
