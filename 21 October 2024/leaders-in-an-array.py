c7723e4dbb9ba547a60a1ed4bcced2999b3648a2
Ash-2k3 <ash.me.cse@gmail.com>
2024-10-21 13:00:35 +0530

    Leaders in an array - Optimal AI

--- 21 October 2024/leaders-in-an-array.py
+++ 21 October 2024/leaders-in-an-array.py
@@ -1,12 +1,11 @@
 class Solution:
     def leaders(self, nums):
-        n = len(nums)
-        maxEleInRight = float('-inf')
-        leaders_list = []
-        for i in range(n - 1, -1, -1):
-            if nums[i] > maxEleInRight:
-                maxEleInRight = nums[i]
-                leaders_list.append(maxEleInRight)
-
-        leaders_list.reverse()
-        return leaders_list
+        leaders = []
+        max_right = float('-inf')
+        
+        for num in reversed(nums):
+            if num >= max_right:
+                leaders.append(num)
+                max_right = num
+        
+        return leaders[::-1]
