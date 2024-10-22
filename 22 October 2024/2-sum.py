846b0af14e3ba725d1d49d935c2f7883200c97d6
Ash-2k3 <ash.me.cse@gmail.com>
2024-10-22 12:42:27 +0530

    2 SUM - Optimal solution using Hashmaps

--- 22 October 2024/2-sum.py
+++ 22 October 2024/2-sum.py
@@ -1,12 +1,15 @@
 class Solution:
     def twoSum(self, nums, target):
         ans = []
-        n = len(nums)
-        for i in range(n):
-            for j in range(i + 1, n):
-                if nums[i] + nums[j] == target:
-                    ans.append(i)
-                    ans.append(j)
-                    return ans
+        map_val_to_index = {}
 
+        for i, val in enumerate(nums):
+            target_val = target - val
+            if target_val in map_val_to_index:
+                ans.append(map_val_to_index[target_val])
+                ans.append(i)
+                return ans
+
+            map_val_to_index[val] = i
+        
         return ans
