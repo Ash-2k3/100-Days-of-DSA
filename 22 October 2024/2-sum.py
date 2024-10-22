60b59fcc6ac1bd71bab31ca9495342a3f24919f8
Ash-2k3 <ash.me.cse@gmail.com>
2024-10-22 12:43:53 +0530

    2 SUM - Minor optimization

--- 22 October 2024/2-sum.py
+++ 22 October 2024/2-sum.py
@@ -1,15 +1,12 @@
 class Solution:
     def twoSum(self, nums, target):
-        ans = []
         map_val_to_index = {}
 
         for i, val in enumerate(nums):
             target_val = target - val
             if target_val in map_val_to_index:
-                ans.append(map_val_to_index[target_val])
-                ans.append(i)
-                return ans
+                return [map_val_to_index[target_val], i]
 
             map_val_to_index[val] = i
-        
-        return ans
+
+        return []
