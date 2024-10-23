1a9cefacfb9581d4c7d5b091f4376ae513dbcf02
Ash-2k3 <ash.me.cse@gmail.com>
2024-10-24 00:26:51 +0530

    4 SUM - Better Hashmap

--- 23 October 2024/4-sum.py
+++ 23 October 2024/4-sum.py
@@ -1,19 +1,22 @@
 class Solution:
     def fourSum(self, nums, target):
         n = len(nums)
-        ans = []
+        ans_set = set()
+
         for i in range(n):
             for j in range(i + 1, n):
+                hash_set = set()
                 for k in range(j + 1, n):
-                    for l in range(k + 1, n):
-                        curr_quad_sum = nums[i] + nums[j] + nums[k] + nums[l]
-                        if curr_quad_sum == target:
-                            quadruplet = [
-                                nums[i],
-                                nums[j],
-                                nums[k],
-                                nums[l]
-                            ]
-                            quadruplet.sort()
-                            ans.append(quadruplet)
-        return ans
+                    fourth_num = target - nums[i] - nums[j] - nums[k]
+                    if fourth_num in hash_set:
+                        quadruplet = [
+                            nums[i],
+                            nums[j],
+                            nums[k],
+                            fourth_num
+                        ]
+                        quadruplet.sort()
+                        ans_set.add(tuple(quadruplet))
+                    hash_set.add(nums[k])
+        
+        return list(ans_set)
