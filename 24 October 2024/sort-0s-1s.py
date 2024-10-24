c1b090771f0a5909abe573a5512097ef22d5e3e3
Ash-2k3 <ash.me.cse@gmail.com>
2024-10-25 01:07:51 +0530

    Sort 0s 1s - Better Approach

--- 24 October 2024/sort-0s-1s.py
+++ 24 October 2024/sort-0s-1s.py
@@ -1,4 +1,23 @@
 class Solution:
     def sortZeroOneTwo(self, nums):
-        nums.sort()
-        return nums
+        ans = [-1 for _ in range(len(nums))]
+        n = len(ans)
+        left_ptr = 0
+        right_ptr = n - 1
+
+        for num in nums:
+            if num == 0:
+                ans[left_ptr] = 0
+                left_ptr += 1
+            elif num == 2:
+                ans[right_ptr] = 2
+                right_ptr -= 1
+        
+        while left_ptr <= right_ptr:
+            ans[left_ptr] = 1
+            left_ptr += 1
+        
+        for i in range(n):
+            nums[i] = ans[i]
+
+        return ans
