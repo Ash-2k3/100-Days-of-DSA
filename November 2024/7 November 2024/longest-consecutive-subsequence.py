a2220c12a73c8547d3ead54aca3162f4daddc282
Ash-2k3 <ash.me.cse@gmail.com>
2024-11-07 15:59:12 +0530

    Longes Consecutive: Optmial Hashset

--- November 2024/7 November 2024/longest-consecutive-subsequence.py
+++ November 2024/7 November 2024/longest-consecutive-subsequence.py
@@ -1,19 +1,18 @@
 class Solution:
     def longestConsecutive(self, nums):
-        nums_set = sorted(set(nums))
+        nums_set = set(nums)
 
-        cnt = 0
-        max_count = 0
-        for i in range(len(nums_set) - 1):
-            if nums_set[i + 1] == nums_set[i] + 1:
-                cnt += 1
-                max_count = max(cnt, max_count)
+        max_count = 1
+
+        for num in nums_set: 
+            if num - 1 in nums_set:
+                continue
             else:
-                cnt = 0
-        
-        return max_count + 1  
+                cnt = 1
+                while num + 1 in nums_set:
+                    cnt += 1
+                    num += 1
+    
+                max_count = max(cnt, max_count)
 
-
-# Line 4: Suggest renaming 'cnt' to 'current_streak' for clarity
-# Line 5: Consider renaming 'max_count' to 'longest_streak' to be more descriptive
-# Line 8-11: Suggest adding a comment explaining the logic of checking and updating consecutive numbers
+        return max_count
