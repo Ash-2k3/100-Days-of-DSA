3b4b10bd81fbaf43e97f5900a828f10e02d4b3c2
Ash-2k3 <ash.me.cse@gmail.com>
2024-11-23 16:03:21 +0530

    Search in rotated array 2: Optimal Approach - We are ignoring the only condition where our logic is failing.

---
+++ November 2024/23 November 2024/search-in-rotated-array-2.py
@@ -0,0 +1,33 @@
+class Solution:
+    def searchInARotatedSortedArrayII(self, nums, k):
+        left = 0
+        right = len(nums) - 1
+
+        while (left <= right):
+            mid = (left + right) // 2
+
+            if nums[mid] == k:
+                return True
+            elif nums[left] == nums[mid] == nums[right]:
+                left += 1
+                right -= 1
+            elif nums[mid] >= nums[left]:
+                # Left Array is sorted
+                if nums[mid] > k and nums[left] <= k:
+                    # Element is indeed in left part of the array.
+                    right = mid - 1
+                else:
+                    # Although array is sorted, the element is not in left portion.
+                    # Search in right portion
+                    left = mid + 1
+            else:
+                # Right part is sorted.
+                if nums[mid] < k and nums[right] >= k:
+                    # Element is indeed in the right part.
+                    left = mid + 1
+                else:
+                    # Although the right part is sorted the element is not here.
+                    right = mid - 1
+
+
+        return False
