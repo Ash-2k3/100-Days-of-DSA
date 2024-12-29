c848bb14dc002480edf78745a2d3c0cca2ca4168
Ash-2k3 <ash.me.cse@gmail.com>
2024-12-29 13:02:08 +0530

    Median of 2 arrays: Better approach - Instead of tracking entire array, just track last two elements being inserted

--- December 2024/29 December 2024/median-of-two-sorted-arrays.py
+++ December 2024/29 December 2024/median-of-two-sorted-arrays.py
@@ -1,33 +1,36 @@
 class Solution:
     def median(self, arr1, arr2):
-        new_arr = []
-
+        new_arr = [0, 0]
+        i = 0
         n = len(arr1) + len(arr2)
         k = n // 2
         ptr1, ptr2 = 0, 0
         while k + 1 and ptr1 < len(arr1) and ptr2 < len(arr2):
             if arr1[ptr1] < arr2[ptr2]:
-                new_arr.append(arr1[ptr1])
+                new_arr[i % 2] = arr1[ptr1]
                 ptr1 += 1
             else:
-                new_arr.append(arr2[ptr2])
+                new_arr[i % 2] = arr2[ptr2]
                 ptr2 += 1
             k -= 1
+            i += 1
 
         while k + 1 and ptr1 < len(arr1):
-            new_arr.append(arr1[ptr1])
+            new_arr[ i % 2] = (arr1[ptr1])
             ptr1 += 1
+            i += 1
             k -= 1
 
         while k + 1 and ptr2 < len(arr2):
-            new_arr.append(arr2[ptr2])
+            new_arr[i % 2] = (arr2[ptr2])
             ptr2 += 1
+            i += 1
             k -= 1
 
         median = 0
         if n % 2 == 0:
             median = (new_arr[-1] + new_arr[-2]) / 2
         else:
-            median = (new_arr[-1])
+            return new_arr[(i - 1) % 2]
         
         return median
